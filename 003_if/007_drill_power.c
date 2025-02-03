5cae3103df33d73ab1216acfb62cb1a53f8a2abb
Dan1 <daniilmarkov3@gmail.com>
2025-02-03 19:37:53 +0500

    drill_changes

--- 003_if/007_drill_power.c
+++ 003_if/007_drill_power.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 int main()
 {
     unsigned int x, y, power;
@@ -8,15 +7,15 @@ int main()
     unsigned int in_square = 0;
 
     printf("Введите x y \n");
-    if(scanf("%u%u", &x, &y) !=2 )
+    if (scanf("%u%u", &x, &y) != 2)
     {
 
         printf("Вы ввели некорректные координаты\n");
         return 1;
-
     }
-    // проверка - в какой части плоскости находится x 
-    if (x <20){
+    // проверка - в какой части плоскости находится x
+    if (x < 20)
+    {
         x_line = 1;
     }
     else if (x <= 50)
@@ -28,59 +27,53 @@ int main()
     {
         x_line = 3;
     }
-    
-// проверка - в квадрате или нет.
+
+    // проверка - в квадрате или нет.
     int is_x_in_square = 40 <= x && x < 80;
     int is_y_in_square = 40 <= y && y < 80;
 
-    if(is_x_in_square && is_y_in_square)
+    if (is_x_in_square && is_y_in_square)
     {
         in_square = 1;
-
     }
 
-
-// Первый участок 30
-    if(x_line == 1){
-
+    switch (x_line)
+    {
+    case 1: // Первый участок
         power = 30;
         printf("Мощность бура должна составлять %u\n", power);
+        break;
 
+    case 2: // Второй участок
+        if (in_square == 1)
+        {
+            power = 50;
+            printf("Мощность бура должна составлять %u\n", power);
+        }
+        else
+        {
+            power = 0;
+            printf("Мощность бура должна составлять %u\n", power);
+        }
+        break;
+
+    case 3: // Третий участок
+        if (in_square == 1)
+        {
+            power = 80;
+            printf("Мощность бура должна составлять %u\n", power);
+        }
+        else
+        {
+            power = 30;
+            printf("Мощность бура должна составлять %u\n", power);
+        }
+        break;
+
+    default:
+        printf("Сюда не должны попасть\n");
+        break;
     }
 
-// Второй участок 0 для y< 40
-
-else if (x_line == 2 && in_square == 1)
-{
-
-    power = 50;
-    printf("Мощность бура должна составлять %u\n", power);
-
-}
-// Второй участок 0 для y< 80
-if(x_line == 2 && in_square == 0)
-{
-
-    power = 0;
-    printf("Мощность бура должна составлять %u\n", power);
-}
-
-if(x_line == 3 && in_square == 1)
-{
-
-    power = 80;
-    printf("Мощность бура должна составлять %u\n", power);
-}
-else if (x_line == 3 && in_square == 0)
-{
-    power = 30;
-    printf("Мощность бура должна составлять %u\n", power);
-}
-
-
-    
-    
-    
     return 0;
-
 }
