f5c0b489bb362219a6bdabf050ab343cc4611294
Dan1 <daniilmarkov3@gmail.com>
2025-02-24 00:46:00 +0500

    24.02

--- 011_dynamic_memory/tasks/04_remove_dublicates.c
+++ 011_dynamic_memory/tasks/04_remove_dublicates.c
@@ -5,16 +5,15 @@
 // и вернуть T or F(contains) функция  Find
 // 1) Резервируем  массив у которого кол-во элементов
 // столько сколько в оригинальном
-// 2) Бежим по нащему ориг массиву и проверяем на каждой иттерации,
+// 2) Бежим по нашему ориг массиву и проверяем на каждой иттерации,
 // если элемент есть в новом массиве через функцию contains,
 // то пропускаем, иначе  добавляем
 
 #include <stdio.h>
-#include "../../library/array_utils.h"
-#include <stdlib.h>
 #include <stdbool.h>
+#include "../../library/array_utils.h"
 
-bool contains(int *arr, int len, int element)
+bool contains(int arr[], size_t len, int element)
 {
     for (int i = 0; i < len; i++)
     {
@@ -25,37 +24,26 @@ bool contains(int *arr, int len, int element)
     }
     return false;
 }
-int *del_duplicates(int *arr, size_t len, size_t *result_len)
+
+void del_dupl(int arr[], size_t len, int arr_result[])
 {
-    int *result = malloc(len * sizeof(int));
-    if (result == NULL)
+    int result_len = 0;
+    for (int i = 0; i < len; i++)
     {
-        return NULL;
-    }
-    size_t len_result = 0;
-    for (size_t i = 0; i < len; i++)
-    {
-        if(contains(result,len_result,arr[i]))
+        if (!contains(arr_result, result_len, arr[i]))
         {
-            result[len_result++] = arr[i];
+            arr_result[result_len++] = arr[i];
         }
     }
-    
-    return result;
 }
 
 int main()
 {
     int arr_1[] = {1, 2, 3, 2, 3, 3, 5, 6, 5};
-    size_t len_arr = sizeof(arr_1) / sizeof(arr_1[0]);
+    int len_arr = sizeof(arr_1) / sizeof(arr_1[0]);
     int result[len_arr];
-    int *arr = malloc(len_arr * sizeof(int));
-    if (arr == NULL)
-    {
-        return 1;
-    }
-    del_duplicates(arr_1,len_arr,result);
-    print_array(arr_1, len_arr);
+    del_dupl(arr_1, len_arr, result);
+    print_array(result, len_arr);
 
     return 0;
 }
